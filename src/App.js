36cc30b7fe837aea0e41707663c753189065da6b
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-08-17 20:01:42 +0530

    Tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+
+  const [meaning, setMeaning] = useState("");
+  const emojiArr= Object.keys(emojiDictionary)
+  
+  function clickHandler(emoji){
+    // setMeaning(event.target.value)
+    // console.log(emoji)
+    setMeaning(emojiDictionary[emoji])
+    
+  }
+
+  function inputHandler(event) {
+    // console.log(emojiDictionary[event.target.value])
+    setMeaning(emojiDictionary[event.target.value])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Bank</h1>
+      <input type="text" className="input--field" placeholder="enter your emoji here" onChange={inputHandler}></input>
+      <h2>{meaning}</h2>
+      <ul>
+        {emojiArr.map((emoji)=>{
+          return(
+            <li key={emoji} onClick={()=>clickHandler(emoji)}>{emoji}</li>
+          )
+        })}
+      </ul>
     </div>
   );
+}
+
+const emojiDictionary = {
+"😀": "Grinning Face",
+"😃": "Grinning Face with Big Eyes",
+"😄": "Grinning Face with Smiling Eyes",
+"😁": "Beaming Face with Smiling Eyes",
+"😆": "Grinning Squinting Face",
+"😅": "Grinning Face with Sweat",
+"🤣": "Rolling on the Floor Laughing",
+"😂": "Face with Tears of Joy",
+"🙂": "Slightly Smiling Face",
+"😉": "Winking Face",
+"😊": "Smiling Face with Smiling Eyes",
+"😇": "Smiling Face with Halo",
+"🥰": "Smiling Face with Hearts",
+"😍": "Smiling Face with Heart-Eyes",
+"🤩": "Star-Struck",
+"😘": "Face Blowing a Kiss",
+"😗": "Kissing Face",
+"☺️": "Smiling Face",
+"😚": "Kissing Face with Closed Eyes",
+"😙": "Kissing Face with Smiling Eyes",
+"🥲": "Smiling Face with Tear",
+"😏": "Smirking Face"
 }
 
 export default App;
